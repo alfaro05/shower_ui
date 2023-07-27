63c826a3cb9d0bea0bbb73da63dfe645e95715f5
Beto <3alberto1alfaro4@gmail.com>
2023-07-26 23:59:07 -0600

    image update

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <BrowserRouter>
       <div className="App">
         <header className="App-header">
-          <img src="/underground.png"/>
+          <img src="/underground.png" width="150px" height="100px"/>
           <h1>¡YA CASI VIENE LEO!</h1>
         </header>
         <NavBar/>
