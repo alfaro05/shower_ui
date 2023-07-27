a2dbcf0737cffc3fb8329b327aa4722cb71496c5
Beto <3alberto1alfaro4@gmail.com>
2023-07-27 00:22:58 -0600

    fixed the welcome page

--- src/Components/Welcome.js
+++ src/Components/Welcome.js
@@ -5,7 +5,10 @@ export const WelcPage = ()=>{
             <p>
                 Nos alegra mucho prepararnos para recibir a Leonardo en las próximas semanas. Queremos compartir
                 este momento con personas especiales, amistades y familia. Con mucho cariño les extendemos la
-                invitación. Por una cuestión de control, para que todo salga de la mejor forma, desplegamos este
+                invitación. 
+            </p>
+            <p>    
+                Por una cuestión de control, para que todo salga de la mejor forma, desplegamos este
                 formulario para que todo sea lo más bonito posible. Por esto es importante confirmar asistencia
                 con la información solicitada. El regalo es lo de menos, pero se muestra un menú desplegable con
                 sugerencias de regalos.
