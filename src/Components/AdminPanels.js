7654591d5a16e9dae87a93cf04518143f286d0f0
Beto <3alberto1alfaro4@gmail.com>
2023-08-08 14:29:29 -0600

    new updates for fetch in admin panel

--- src/Components/AdminPanels.js
+++ src/Components/AdminPanels.js
@@ -12,7 +12,10 @@ export const PeoplePanel =()=>{
           }
         })
         .then((response)=>{
-            console.log(response);
+            return(response.json())
+        })
+        .then((data)=>{
+            console.log(data);
         })
     })
     return(
